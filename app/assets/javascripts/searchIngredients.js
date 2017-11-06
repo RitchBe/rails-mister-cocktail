f8eafc1201a12f9877fc937f4ee72b9696daf379
Richard Houet <itchy.ooc@gmail.com>
2017-11-06 13:06:43 +0000

    Change ingredient list + working

---
+++ app/assets/javascripts/searchIngredients.js
@@ -0,0 +1,36 @@
+function searchIngredient() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('dose_ingredient_id');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("ingredients-list");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        p = li[i].getElementsByTagName("p")[0];
+        if (p.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+
+    };
+
+};
+
+    // var li = document.getElementByClassName("list")
+
+
+
+
+li = document.getElementsByClassName("list");
+
+document.addEventListener('DOMContentLoaded', function(){
+    for (var i = 0 ; i < li.length; i++) {
+   li[i].addEventListener('click' , (event) => {
+    console.log(event.target.innerText)
+    document.getElementById('dose_ingredient_id').value = event.target.innerText;
+   });
+}
+}, false);
